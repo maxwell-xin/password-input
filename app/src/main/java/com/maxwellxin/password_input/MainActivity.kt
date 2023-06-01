965fe48e70de1e2420899900f75aabf2e6a85f8f
XinHere <siangheng.ang@wiseyes.net>
2023-06-01 17:38:13 +0800

    Support jetpack compose

--- app/src/main/java/com/maxwellxin/password_input/MainActivity.kt
+++ app/src/main/java/com/maxwellxin/password_input/MainActivity.kt
@@ -1,26 +1,71 @@
 package com.maxwellxin.password_input
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowCompat
-import com.maxwellxin.password_input.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    //Variable
+    private var _password = MutableStateFlow(InputContent("", null, false))
+    private var password = _password.asStateFlow()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.composeView.setContent {
-            MaterialTheme {
-                Text(text = "Hello in Compose World!")
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
+                    AndroidView(
+                        modifier = Modifier
+                            .height(50.dp)
+                            .fillMaxWidth(), // Occupy the max size in the Compose UI tree
+                        factory = { context ->
+                            PasswordInput(context).apply {}
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                    PasswordInputComponent(
+                        label = "Password",
+                        hint = "Enter Password",
+                        inputContent = password.collectAsState().value,
+                        labelColor = colorResource(android.R.color.darker_gray),
+                        borderColor = colorResource(android.R.color.darker_gray),
+                        onValueChange = {
+                            _password.value = InputContent(
+                                it,
+                                password.value.error,
+                                password.value.visible
+                            )
+                        },
+                        onVisibleChange = {
+                            _password.value = InputContent(
+                                password.value.value,
+                                password.value.error,
+                                it
+                            )
+                        }
+                    )
+                }
             }
         }
     }
---
+++ password-input/src/main/java/com/maxwellxin/password_input/PasswordInputComponent.kt
@@ -0,0 +1,119 @@
+package com.maxwellxin.password_input
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class InputContent(
+    var value: String = "",
+    var error: String? = null,
+    var visible: Boolean = false,
+)
+
+@Composable
+fun PasswordInputComponent(
+    label: String?,
+    hint: String,
+    fontSize: TextUnit = 14.sp,
+    labelColor: Color,
+    borderColor: Color,
+    fontFamily: FontFamily? = null,
+    inputContent: InputContent,
+    onValueChange: (String) -> Unit,
+    onVisibleChange: (Boolean) -> Unit,
+) {
+    fontFamily ?: FontFamily(
+        listOf(
+            Font(R.font.dancing_script_bold)
+        )
+    )
+
+    Column {
+        if (label != null)
+            Text(
+                text = label,
+                color = labelColor,
+                fontFamily = fontFamily,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp)
+            )
+        BasicTextField(
+            value = inputContent.value,
+            onValueChange = onValueChange,
+            maxLines = 1,
+            singleLine = true,
+            visualTransformation = if (inputContent.visible) VisualTransformation.None else PasswordVisualTransformation(),
+            textStyle = TextStyle.Default.copy(
+                fontSize = fontSize,
+                fontFamily = fontFamily,
+            ),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Color.Transparent,
+                            shape = RoundedCornerShape(size = 8.dp)
+                        )
+                        .border(
+                            width = 1.dp,
+                            color = if (inputContent.error == null) borderColor else MaterialTheme.colors.error,
+                            shape = RoundedCornerShape(size = 8.dp)
+                        )
+                        .padding(all = 12.dp),
+                ) {
+                    Box(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        if (inputContent.value.isEmpty())
+                            Text(
+                                text = hint,
+                                fontSize = fontSize,
+                                fontFamily = fontFamily,
+                                color = colorResource(android.R.color.darker_gray),
+                            )
+                        innerTextField.invoke()
+                    }
+                    Icon(
+                        painter = if (inputContent.visible) painterResource(R.drawable.icon_eyes) else painterResource(R.drawable.icon_close_eyes),
+                        contentDescription = "",
+                        modifier = Modifier.clickable(onClick = {
+                            onVisibleChange(!inputContent.visible)
+                        }),
+                    )
+                }
+            }
+        )
+        if (inputContent.error != null)
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = inputContent.error!!,
+                fontSize = 12.sp,
+                color = MaterialTheme.colors.error
+            )
+    }
+}
